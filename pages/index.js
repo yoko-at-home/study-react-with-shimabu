71d0539daffea01e79478e4d3a8fb115d84787ed
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-04-19 13:31:10 +0900

    ♻️

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import {Footer} from '../components/Footer'
 import { Links } from '../components/Links'
-import Headline from '../components/Headline';
+import { Headline } from '../components/Headline';
 
 export default function Home() {
   return (
