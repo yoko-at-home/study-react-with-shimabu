c4bba1f4aae1dfc9f170e4109b2a6f2d3bec8d19
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-04-26 08:58:26 +0900

    ✨ 講座用のuseEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-// import { useEffect, useCallback } from 'react';
+import { useEffect, useCallback } from 'react';
 
   // const handleClick = (e, foo) => {
   //   e.preventDefault();
